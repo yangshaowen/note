18411de1ae39ce14a279472f0ca7cab9ad58124e
yangshaowen <1165166678@qq.com>
2020-12-25 22:42:47 +0800

    2020-12-25 22:42:47

---
+++ java/并发/com/code/container/ConcurrentMapTest.java
@@ -0,0 +1,20 @@
+package com.code.container;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * @Description
+ * @auther 阳少文
+ * @create 2020-12-25 22:16
+ */
+public class ConcurrentMapTest {
+
+
+    public static void main(String[] args) {
+        ConcurrentMap map = new ConcurrentHashMap<>();
+
+
+    }
+
+}
