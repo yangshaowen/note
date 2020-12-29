6509683631dec79528b2111cd20b7c73c73435ed
yangshaowen <1165166678@qq.com>
2020-12-29 23:10:00 +0800

    2020-12-29 23:10:00

--- java/并发/com/code/thread/ThreadTest.java
+++ java/并发/com/code/thread/ThreadTest.java
@@ -8,45 +8,45 @@ package com.code.thread;
 public class ThreadTest {
 
     public static void main(String[] args) throws Exception {
-        new Thread(()->{
+        new Thread(() -> {
             System.out.println("构造方法,Lambda启动的线程,使用了默认的线程名称: " + Thread.currentThread().getName());
         }).start();
 
 
-        new Thread(()->{
+        new Thread(() -> {
             System.out.println("构造方法,Lambda启动的线程,使用了自定义的名称: " + Thread.currentThread().getName());
-        },"thrad-自定义的名称").start();
+        }, "thrad-自定义的名称").start();
 
         //毫秒
         Thread.sleep(1000);
         //毫秒,纳秒
-        Thread.sleep(1000,1000);
+        Thread.sleep(1000, 1000);
 
         //交出CPU,进入就绪状态
         Thread.yield();
 
         Thread join = new Thread(() -> {
             try {
-                System.out.println(Thread.currentThread().getName()+"开始执行");
+                System.out.println(Thread.currentThread().getName() + "开始执行");
                 Thread.sleep(2000);
-                System.out.println(Thread.currentThread().getName()+"执行结束");
+                System.out.println(Thread.currentThread().getName() + "执行结束");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        },"Thread-join");
+        }, "Thread-join");
         join.start();
 
         //等待
-        System.out.println(Thread.currentThread().getName()+"准备等待"+join.getName()+"结束");
+        System.out.println(Thread.currentThread().getName() + "准备等待" + join.getName() + "结束");
         join.join();
-        System.out.println(join.getName()+"直接结束, "+Thread.currentThread().getName()+"继续执行");
+        System.out.println(join.getName() + "直接结束, " + Thread.currentThread().getName() + "继续执行");
 
         //Runnable
         new Thread(new RunnableTest()).start();
 
     }
 
-    public static class RunnableTest implements Runnable{
+    public static class RunnableTest implements Runnable {
 
         @Override
         public void run() {
